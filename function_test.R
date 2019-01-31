e812fcb1df95aec4f8a7550b881899ada767ceba
Elitsa Stoyanova <estoyanova@rockefeller.edu>
2019-01-31 11:48:09 -0500

    update

--- function_test.R
+++ function_test.R
@@ -1,9 +1,12 @@
 library(Seurat)
 library(tidyverse)
 
-bp_genes = read.delim("broad_peak_anno_genes.txt", header = FALSE)
+bp_genes = read.delim("broad_peak_anno_NO_dmv_genes.txt", header = FALSE)
 bp_dmv_genes = read.delim("broad_peak_anno_dmv_genes.txt", header = FALSE)
 top95_genes = read.delim("top95_genes.txt", header = FALSE)
+top2k_no_dmv = read.delim("top2k_no_dmv_broadpeak_genes.txt", header = FALSE)
+top1k_no_dmv = read.delim("top1k_no_dmv_broadpeak_genes.txt", header = FALSE)
+dmv_noaccsb = read.delim("noaccsb_dmv_genes.txt", header = TRUE)
 
 sample_sheet = read.table("cerebellum_cell_metadata.tsv", sep = "\t", header = TRUE)
 files = list.files("~/Documents/data/")
@@ -30,7 +33,7 @@ seurat_processing = function(folder, sample_sheet) {
   return(seu_object)
 }
 
-timepoint_variance = function(folder, sample_sheet) {
+timepoint_variance = function(folder, sample_sheet, stats_func) {
   seu_object = seurat_processing(folder, sample_sheet)
   
   # select only purkinje cells
@@ -41,34 +44,60 @@ timepoint_variance = function(folder, sample_sheet) {
   
   # calculate variance
   bp_dmv_genes_var = seu_object_purk_df %>% filter(rowname %in% bp_dmv_genes$V1) %>%
-    select_if(is.numeric) %>% apply(1, cv)
+    select_if(is.numeric) %>% apply(1, stats_func)
   
   bp_genes_var = seu_object_purk_df %>% filter(rowname %in% bp_genes$V1) %>%
-    select_if(is.numeric) %>% apply(1, cv)
+    select_if(is.numeric) %>% apply(1, stats_func)
   
   top95_genes_var = seu_object_purk_df %>% filter(rowname %in% top95_genes$V1) %>%
-    select_if(is.numeric) %>% apply(1, cv)
+    select_if(is.numeric) %>% apply(1, stats_func)
+  
+  top2k_no_dmv = seu_object_purk_df %>% filter(rowname %in% top2k_no_dmv$V1) %>%
+    select_if(is.numeric) %>% apply(1, stats_func)
+  
+  top1k_no_dmv = seu_object_purk_df %>% filter(rowname %in% top1k_no_dmv$V1) %>%
+    select_if(is.numeric) %>% apply(1, stats_func)
+  
+  dmv_noaccsb = seu_object_purk_df %>% filter(rowname %in% dmv_noaccsb$V1) %>%
+    select_if(is.numeric) %>% apply(1, stats_func)
   
   bound = bind_rows(
     tibble(val = bp_dmv_genes_var, var = 'bp_dmv'),
     tibble(val = bp_genes_var, var = 'bp'),
-    tibble(val = top95_genes_var, var = 'top95')) %>% add_column(timepoint = folder)
+    tibble(val = top95_genes_var, var = 'top95'),
+    tibble(val = top2k_no_dmv, var = 'top2k'),
+    tibble(val = top1k_no_dmv, var = 'top1k'),
+    tibble(val = dmv_noaccsb, var = 'dmv_noaccsb')) %>% add_column(timepoint = folder)
   
   return(bound)
 }
 
 cv = function(x){ sd(x)/mean(x) }
 
-means = map_dfr(files, ~timepoint_variance(., sample_sheet))
+means = map_dfr(files, ~timepoint_variance(., sample_sheet, mean))
 means$timepoint = substr(means$timepoint,1,nchar(means$timepoint)-1)
 pdf("means_sn_cb.pdf", 10, 3)
-ggplot(means) + geom_boxplot(aes(var, val, fill = var, color = var), outlier.shape = NA) + facet_wrap(~timepoint, nrow = 1) +
-  scale_x_discrete(limits = c("bp_dmv", "bp", "top95")) + ylim(0,1)
+ggplot(means) + geom_boxplot(aes(var, val, fill = var, color = var), 
+                             outlier.shape = NA, alpha = 0.2) + 
+  facet_wrap(~timepoint, nrow = 1) +
+  scale_x_discrete(limits = c("top1k", "top2k", "bp_dmv", "bp", "top95")) + ylim(0,1)
 dev.off()
 
-variances = map_dfr(files, ~timepoint_variance(., sample_sheet))
+variances = map_dfr(files, ~timepoint_variance(., sample_sheet, cv))
 variances$timepoint = substr(variances$timepoint,1,nchar(variances$timepoint)-1)
 pdf("cv_sn_cb.pdf", 10, 3)
-ggplot(variances) + geom_boxplot(aes(var, val, fill = var, color = var), outlier.shape = NA) + facet_wrap(~timepoint, nrow = 1) +
-  scale_x_discrete(limits = c("bp_dmv", "bp", "top95")) + ylim(0, 5)
+ggplot(variances) + geom_boxplot(aes(var, val, fill = var, color = var),
+                                 alpha = 0.2, outlier.shape = NA) + 
+  facet_wrap(~timepoint, nrow = 1) +
+  scale_x_discrete(limits = c("top1k", "top2k", "bp_dmv", "bp", "top95")) + ylim(0, 5)
 dev.off()
+
+summary(means %>% filter(var=='bp') %>% select(val))
+summary(means %>% filter(var=='bp_dmv') %>% select(val))
+
+summary(variances%>% filter(var=='top2k') %>% select(val))
+
+summary(variances%>% filter(var=='bp_dmv') %>% select(val))
+test = as.data.frame(variances%>% filter(var=='bp_dmv') %>% select(val))
+
+wilcox.test(test$val, mu = 1.8846)
